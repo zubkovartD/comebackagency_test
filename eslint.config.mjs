c3e69220bf64e34b4899e9fbeefb9a0db34adc18
Artem Zubkov <zubkov.artem46@gmail.com>
2025-07-30 21:10:20 +0300

    added prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "no-console": "error",
+      "no-var": "error",
+      eqeqeq: "error",
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
